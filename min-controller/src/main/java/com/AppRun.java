45cbe5dd1fef2cac7a31a7f03a977ddfd6b3feb8
commonsstring <m838206069@163.com>
2019-04-22 20:33:50 +0800

    add

--- min-controller/src/main/java/com/AppRun.java
+++ min-controller/src/main/java/com/AppRun.java
@@ -1,9 +1,11 @@
 package com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@MapperScan("com.module.system.mapper")
 @SpringBootApplication
 @EnableTransactionManagement
 public class AppRun {
